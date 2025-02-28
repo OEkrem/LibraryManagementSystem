2fb597f86a2aa206891a5651c86352b57fbc26f7
Ekrem <oekremyildirim@outlook.com>
2025-02-28 18:43:50 +0300

    init(gateway): Add Gateway-Book operations

--- book-service/src/main/java/com/oekrem/mikroservices/mapper/BookMapper.java
+++ book-service/src/main/java/com/oekrem/mikroservices/mapper/BookMapper.java
@@ -5,15 +5,10 @@ import com.oekrem.mikroservices.dto.CreateBookRequest;
 import com.oekrem.mikroservices.dto.UpdateBookRequest;
 import com.oekrem.mikroservices.model.Book;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface BookMapper {
-
-    BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
-
     BookResponse toResponse(Book book);
     Book toBookFromCreateRequest(CreateBookRequest createBookRequest);
     Book toBookFromUpdateRequest(UpdateBookRequest updateBookRequest);
-
 }
