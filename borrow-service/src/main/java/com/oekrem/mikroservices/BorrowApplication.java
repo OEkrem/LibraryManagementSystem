a0fe4bfb6b3388f69cfb33ef5d976498ca80638d
Ekrem <oekremyildirim@outlook.com>
2025-03-05 18:29:32 +0300

    feat(borrow-service): pom.xml,properties and config files updated

--- borrow-service/src/main/java/com/oekrem/mikroservices/BorrowApplication.java
+++ borrow-service/src/main/java/com/oekrem/mikroservices/BorrowApplication.java
@@ -1,13 +1,13 @@
 package com.oekrem.mikroservices;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BorrowApplication
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(BorrowApplication.class, args);
     }
 }
